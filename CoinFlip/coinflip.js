0c27969d9e1148c0332dad828919b0a042f71cb5
JJ <99203771+TheUnknown404@users.noreply.github.com>
2023-12-13 11:17:31 +0000

    Add files via upload
    - Added CSS
    - Updated JS code
    - Added comments to JS code

--- CoinFlip/coinflip.js
+++ CoinFlip/coinflip.js
@@ -1,52 +1,83 @@
-let playerPick; // Define playerPick outside the click event listeners
-
+// Retrieve the score from localStorage or set default values
 let score = JSON.parse(localStorage.getItem("score")) || {
-    wins: 0,
-    losses: 0,
-  };
+  wins: 0,
+  losses: 0,
+};
 
-document.getElementById('heads').addEventListener('click', () => {
-    document.getElementById("text").innerHTML = 'You picked: heads'
-    playerPick = 'heads';
-    const computerPick = game(); // Get the computerPick from the game function
-    result(playerPick, computerPick);
+// Declare playerPick variable
+let playerPick;
+
+// Event listener for the 'heads' button
+document.getElementById("heads").addEventListener("click", () => {
+  // Update UI to show player's pick
+  document.getElementById("text").innerHTML = "You picked: heads";
+  // Set playerPick to 'heads'
+  playerPick = "heads";
+  // Get computer's pick
+  const computerPick = game();
+  // Determine and display the result
+  result(playerPick, computerPick);
 });
 
-document.getElementById('tails').addEventListener('click', () => {
-    document.getElementById("text").innerHTML = 'You picked: tails'
-    playerPick = 'tails';
-    const computerPick = game(); // Get the computerPick from the game function
-    result(playerPick, computerPick);
+// Event listener for the 'tails' button
+document.getElementById("tails").addEventListener("click", () => {
+  // Update UI to show player's pick
+  document.getElementById("text").innerHTML = "You picked: tails";
+  // Set playerPick to 'tails'
+  playerPick = "tails";
+  // Get computer's pick
+  const computerPick = game();
+  // Determine and display the result
+  result(playerPick, computerPick);
 });
 
+// Function to simulate the computer's pick
 function game() {
-    const computerPick = Math.random() > 1 / 2 ? 'heads' : 'tails';
-    document.getElementById('text2').innerHTML = `Computer Picked: ${computerPick}`;
-    return computerPick; // Return the computerPick value
+  // Randomly choose 'heads' or 'tails' for the computer
+  const computerPick = Math.random() > 1 / 2 ? "heads" : "tails";
+  // Update UI to show computer's pick
+  document.getElementById(
+    "text2"
+  ).innerHTML = `Computer Picked: ${computerPick}`;
+  return computerPick;
 }
 
+// Function to determine the result and update the score
 function result(playerPick, computerPick) {
-    if (playerPick === computerPick) {
-        score.wins++;
-    } else {
-        score.losses++;
-    }
-    updateScore()
+  // Compare player's pick with computer's pick
+  if (playerPick === computerPick) {
+    // Player wins
+    score.wins++;
+    document.getElementById("heading").innerHTML = "You Win!";
+  } else {
+    // Player loses
+    score.losses++;
+    document.getElementById("heading").innerHTML = "You Lose!";
+  }
+  // Update the displayed score
+  updateScore();
 }
 
+// Function to update the displayed score and store it in localStorage
 function updateScore() {
-document.getElementById('wins').innerHTML = score.wins;
-document.getElementById('losses').innerHTML = score.losses;
-localStorage.setItem('score', JSON.stringify(score));
+  // Update the displayed wins and losses
+  document.getElementById("wins").innerHTML = score.wins;
+  document.getElementById("losses").innerHTML = score.losses;
+  // Store the score in localStorage
+  localStorage.setItem("score", JSON.stringify(score));
 }
 
+// Initial update of the score
 updateScore();
 
+// Event listener for the 'reset' button
+document.getElementById("reset").addEventListener("click", reset);
 
-document.getElementById('reset').addEventListener('click', reset)
-
+// Function to reset the score
 function reset() {
-score.wins = 0;
-score.losses = 0;
-updateScore();
+  // Reset wins and losses to zero
+  score.wins = 0;
+  score.losses = 0;
+  // Update the displayed score
+  updateScore();
 }
